61f2d9d1b40d4b743b13ca56e286e7b72482a9e8
trishund <trishundkupatkar@gmail.com>
2025-12-03 20:23:09 +0530

    Fix: Remove conflicting SSL plugin for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        basicSsl(),
         VitePWA({
             registerType: 'autoUpdate',
             includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
@@ -33,7 +30,6 @@ export default defineConfig({
         })
     ],
     server: {
-        host: true,
-        https: true
+        host: true
     }
 })
